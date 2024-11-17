2fe3b5590936912a680c6e184f49719768c86674
dimnov <dimnov1512@gmail.com>
2024-11-17 21:12:56 +0200

    import the images

--- src/app/components/Footer/FooterCopyright/FooterCopyright.js
+++ src/app/components/Footer/FooterCopyright/FooterCopyright.js
@@ -1,24 +1,30 @@
 import Image from "next/image";
 import styles from "./FooterCopyright.module.css";
 
-const images = ["Visa.png", "MasterCard.png", "PayPal.png", "ApplePay.png", "GooglePay.png"];
+import visa from "/public/images/payment-methods/Visa.png";
+import mastercard from "/public/images/payment-methods/MasterCard.png";
+import paypal from "/public/images/payment-methods/PayPal.png";
+import applepay from "/public/images/payment-methods/ApplePay.png";
+import googlepay from "/public/images/payment-methods/GooglePay.png";
+
+const paymentMethods = [
+  { src: visa, alt: "Visa Logo" },
+  { src: mastercard, alt: "MasterCard Logo" },
+  { src: paypal, alt: "PayPal Logo" },
+  { src: applepay, alt: "Apple Pay Logo" },
+  { src: googlepay, alt: "Google Pay Logo" },
+];
 
 function FooterCopyright() {
   return (
-    <div className={styles.copyright}>
+    <footer className={styles.copyright}>
       <p>Outfitter &copy;2000-2024, All Rights Reserved</p>
       <div className={styles.payment_methods}>
-        {images.map((image) => (
-          <Image
-            key={image}
-            width={66}
-            height={49}
-            src={`/images/payment-methods/${image}`}
-            alt={image}
-          />
+        {paymentMethods.map((method, index) => (
+          <Image key={index} layout="intrinsic" src={method.src} alt={method.alt} />
         ))}
       </div>
-    </div>
+    </footer>
   );
 }
 
