89491604a22891a26da5f9f2b26447f4ebb6cbda
Dimitar Ivanov <dimitar.ivanov.dev@gmail.com>
2024-11-25 01:41:20 +0200

    Create HomeProducts component

---
+++ src/app/components/Home/HomeProducts/HomeProducts.js
@@ -0,0 +1,82 @@
+import styles from "./HomeProducts.module.css";
+import Image from "next/image";
+
+import tshirt from "/public/images/products/tshirt.png";
+
+function HomeProducts() {
+  return (
+    <section className={styles.home_products}>
+      {/* NEW ARRIVALS */}
+      <div className={styles.box_products}>
+        <p className={styles.box_products_title}>NEW ARRIVALS</p>
+        <ul className={styles.box_products_list}>
+          <li className={styles.product_item}>
+            <Image layout="intrinsic" src={tshirt} alt={"asd"} />
+            <p className={styles.product_item_title}>T-shirt with Tape Details</p>
+            {/* <p className={styles.product_item_score}>score</p> */}
+            <p className={styles.product_item_price}>$120</p>
+          </li>
+          <li className={styles.product_item}>
+            <Image layout="intrinsic" src={tshirt} alt={"asd"} />
+            <p className={styles.product_item_title}>T-shirt with Tape Details</p>
+            {/* <p className={styles.product_item_score}>score</p> */}
+            <p className={styles.product_item_price}>$120</p>
+          </li>
+          <li className={styles.product_item}>
+            <Image layout="intrinsic" src={tshirt} alt={"asd"} />
+            <p className={styles.product_item_title}>T-shirt with Tape Details</p>
+            {/* <p className={styles.product_item_score}>score</p> */}
+            <p className={styles.product_item_price}>$120</p>
+          </li>
+          <li className={styles.product_item}>
+            <Image layout="intrinsic" src={tshirt} alt={"asd"} />
+            <p className={styles.product_item_title}>T-shirt with Tape Details</p>
+            {/* <p className={styles.product_item_score}>score</p> */}
+            <p className={styles.product_item_price}>$120</p>
+          </li>
+        </ul>
+        <a href="#" className={styles.view_all_button}>
+          View All
+        </a>
+      </div>
+      <div className={styles.brake}>
+        <hr />
+      </div>
+      {/* TOP SELLING */}
+      <div className={styles.box_products}>
+        <p className={styles.box_products_title}>TOP SELLING</p>
+        <ul className={styles.box_products_list}>
+          <li className={styles.product_item}>
+            <Image layout="intrinsic" src={tshirt} alt={"asd"} />
+            <p className={styles.product_item_title}>T-shirt with Tape Details</p>
+            {/* <p className={styles.product_item_score}>score</p> */}
+            <p className={styles.product_item_price}>$120</p>
+          </li>
+          <li className={styles.product_item}>
+            <Image layout="intrinsic" src={tshirt} alt={"asd"} />
+            <p className={styles.product_item_title}>T-shirt with Tape Details</p>
+            {/* <p className={styles.product_item_score}>score</p> */}
+            <p className={styles.product_item_price}>$120</p>
+          </li>
+          <li className={styles.product_item}>
+            <Image layout="intrinsic" src={tshirt} alt={"asd"} />
+            <p className={styles.product_item_title}>T-shirt with Tape Details</p>
+            {/* <p className={styles.product_item_score}>score</p> */}
+            <p className={styles.product_item_price}>$120</p>
+          </li>
+          <li className={styles.product_item}>
+            <Image layout="intrinsic" src={tshirt} alt={"asd"} />
+            <p className={styles.product_item_title}>T-shirt with Tape Details</p>
+            {/* <p className={styles.product_item_score}>score</p> */}
+            <p className={styles.product_item_price}>$120</p>
+          </li>
+        </ul>
+        <a href="#" className={styles.view_all_button}>
+          View All
+        </a>
+      </div>
+    </section>
+  );
+}
+
+export default HomeProducts;
