2536560f39cff185a4e5311232d77f795d599095
Dimitar Ivanov <dimitar.ivanov.dev@gmail.com>
2024-11-25 22:22:17 +0200

    Import Categories to Home page

--- src/app/pages/HomePage/HomePage.js
+++ src/app/pages/HomePage/HomePage.js
@@ -1,9 +1,9 @@
-import Image from "next/image";
 import styles from "./HomePage.module.css";
 
 import Hero from "@/app/components/Home/Hero/Hero";
 import Brands from "@/app/components/Home/Brands/Brands";
 import HomeProducts from "@/app/components/Home/HomeProducts/HomeProducts";
+import Categories from "@/app/components/Home/Categories/Categories";
 
 function HomePage() {
   return (
@@ -11,25 +11,7 @@ function HomePage() {
       <Hero />
       <Brands />
       <HomeProducts />
-      <section className={styles.section_categories}>
-        <div className={styles.categories_container}>
-          <header className={styles.categories_header}>BROWSE BY DRESS STYLE</header>
-          <div className={styles.categories_grid}>
-            <div className={styles.category_box}>
-              <p>Casual</p>
-            </div>
-            <div className={styles.category_box}>
-              <p>Formal</p>
-            </div>
-            <div className={styles.category_box}>
-              <p>Party</p>
-            </div>
-            <div className={styles.category_box}>
-              <p>Gym</p>
-            </div>
-          </div>
-        </div>
-      </section>
+      <Categories />
     </main>
   );
 }
