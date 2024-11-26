08deea4af6838bd2ad032e96bdc7de4ba8b4ae54
Dimitar Ivanov <dimitar.ivanov.dev@gmail.com>
2024-11-26 20:36:40 +0200

    remove unnecessary code

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,5 @@
-import Head from "next/head";
-
-import Footer from "./components/Footer/Footer";
-import Header from "./components/Header/Header";
+import Footer from "@/app/components/Footer/Footer";
+import Header from "@/app/components/Header/Header";
 
 import "./fonts/satoshi.css";
 import "./fonts/integralCF.css";
@@ -13,19 +11,21 @@ export const metadata = {
     "Shop trendy clothing, shoes, and accessories at Outfitter. New arrivals every season!",
   keywords:
     "clothing, fashion, online store, men's fashion, women's fashion, accessories, trendy clothes",
-  author: "Outfitter",
+  authors: [{ name: "Outfitter" }],
+  icons: {
+    icon: [
+      { rel: "icon", type: "image/png", sizes: "16x16", url: "/favicon-16x16.png" },
+      { rel: "icon", type: "image/png", sizes: "32x32", url: "/favicon-32x32.png" },
+      { rel: "apple-touch-icon", sizes: "180x180", url: "/apple-icon-180x180.png" },
+    ],
+    other: [{ rel: "icon", type: "image/png", sizes: "192x192", url: "/android-icon-192x192.png" }],
+  },
+  manifest: "/manifest.json",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png" />
-        <link rel="icon" type="image/png" sizes="192x192" href="/android-icon-192x192.png" />
-      </Head>
       <body>
         <Header />
         <main>{children}</main>
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,17 @@
-import HomePage from "./pages/HomePage/HomePage";
+import Hero from "@/app/components/Home/Hero/Hero";
+import Brands from "@/app/components/Home/Brands/Brands";
+import HomeProducts from "@/app/components/Home/HomeProducts/HomeProducts";
+import Categories from "@/app/components/Home/Categories/Categories";
+import Customers from "@/app/components/Home/Customers/Customers";
 
 export default function Home() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <>
+      <Hero />
+      <Brands />
+      <HomeProducts />
+      <Categories />
+      <Customers />
+    </>
   );
 }
