47b8844c5d3fc00c4ecb7ab4284c7a3858859a2d
Dimitar Ivanov <dimitar.ivanov.dev@gmail.com>
2024-11-29 01:28:47 +0200

    Create Breadcrumbs component

---
+++ src/app/components/Catalog/Breadcrumb/Breadcrumb.js
@@ -0,0 +1,12 @@
+import styles from "./Breadcrumb.module.css";
+
+function Breadcrumb() {
+  return (
+    <div className={styles.breadcrumb}>
+      <p>Home</p>
+      <p>Casual</p>
+    </div>
+  );
+}
+
+export default Breadcrumb;
