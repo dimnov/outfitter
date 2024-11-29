32ef9700532f6ab4d635e09382934330ae8fd62b
Dimitar Ivanov <dimitar.ivanov.dev@gmail.com>
2024-11-29 22:56:24 +0200

    Create ProductBox component

--- src/app/components/Catalog/Products/ProductBox/ProductBox.js
+++ src/app/components/Catalog/Products/ProductBox/ProductBox.js
@@ -9,8 +9,79 @@ function ProductBox() {
       <Image src={tshirt} layout="intrinsic" alt="tshirt" />
       <div className={styles.product_info}>
         <p className={styles.product_title}>Gradient Graphic T-shirt</p>
-        {/* score */}
-        <p className={styles.product_price}>$145</p>
+        <div className={styles.score_box}>
+          <div className={styles.score_stars}>
+            <svg
+              width="19"
+              height="17"
+              viewBox="0 0 19 17"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M9.24494 0.255005L11.8641 5.89491L18.0374 6.6431L13.4829 10.8769L14.679 16.9793L9.24494 13.956L3.8109 16.9793L5.00697 10.8769L0.452479 6.6431L6.62573 5.89491L9.24494 0.255005Z"
+                fill="#FFC633"
+              />
+            </svg>
+            <svg
+              width="19"
+              height="17"
+              viewBox="0 0 19 17"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M9.24494 0.255005L11.8641 5.89491L18.0374 6.6431L13.4829 10.8769L14.679 16.9793L9.24494 13.956L3.8109 16.9793L5.00697 10.8769L0.452479 6.6431L6.62573 5.89491L9.24494 0.255005Z"
+                fill="#FFC633"
+              />
+            </svg>
+            <svg
+              width="19"
+              height="17"
+              viewBox="0 0 19 17"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M9.24494 0.255005L11.8641 5.89491L18.0374 6.6431L13.4829 10.8769L14.679 16.9793L9.24494 13.956L3.8109 16.9793L5.00697 10.8769L0.452479 6.6431L6.62573 5.89491L9.24494 0.255005Z"
+                fill="#FFC633"
+              />
+            </svg>
+            <svg
+              width="19"
+              height="17"
+              viewBox="0 0 19 17"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M9.24494 0.255005L11.8641 5.89491L18.0374 6.6431L13.4829 10.8769L14.679 16.9793L9.24494 13.956L3.8109 16.9793L5.00697 10.8769L0.452479 6.6431L6.62573 5.89491L9.24494 0.255005Z"
+                fill="#FFC633"
+              />
+            </svg>
+            <svg
+              width="19"
+              height="17"
+              viewBox="0 0 19 17"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M9.24494 0.255005L11.8641 5.89491L18.0374 6.6431L13.4829 10.8769L14.679 16.9793L9.24494 13.956L3.8109 16.9793L5.00697 10.8769L0.452479 6.6431L6.62573 5.89491L9.24494 0.255005Z"
+                fill="#FFC633"
+              />
+            </svg>
+          </div>
+          <p className={styles.score_number}>5.0/5</p>
+        </div>
+        <div className={styles.product_price_box}>
+          <p className={styles.product_price}>$145</p>
+
+          {/* if discount */}
+          <p className={styles.product_price_discount}>$160</p>
+          <p className={styles.discount_percentage}>-30%</p>
+          {/* if discount */}
+        </div>
       </div>
     </div>
   );
