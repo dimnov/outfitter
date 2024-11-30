fc6fa7f977b7c13d78eb486821199345925c6a89
Dimitar Ivanov <dimitar.ivanov.dev@gmail.com>
2024-11-30 19:36:24 +0200

    export supabase

--- src/app/lib/supabase.js
+++ src/app/lib/supabase.js
@@ -3,4 +3,4 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseKey = process.env.SUPABASE_KEY;
 const supabaseUrl = process.env.SUPABASE_URL;
 
-const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseKey);
