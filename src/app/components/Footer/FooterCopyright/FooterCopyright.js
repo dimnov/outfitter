d2ada11ea5a9e6a70d4da0e69f6747371e219598
Dimitar Ivanov <dimitar.ivanov.dev@gmail.com>
2024-12-02 00:32:42 +0200

    Update FooterCopyright.js

--- src/app/components/Footer/FooterCopyright/FooterCopyright.js
+++ src/app/components/Footer/FooterCopyright/FooterCopyright.js
@@ -1,28 +1,12 @@
-import Image from "next/image";
+import PaymentMethods from "../../Icons/PaymentMethods";
 import styles from "./FooterCopyright.module.css";
-
-import visa from "/public/images/payment-methods/Visa.png";
-import mastercard from "/public/images/payment-methods/MasterCard.png";
-import paypal from "/public/images/payment-methods/PayPal.png";
-import applepay from "/public/images/payment-methods/ApplePay.png";
-import googlepay from "/public/images/payment-methods/GooglePay.png";
-
-const paymentMethods = [
-  { src: visa, alt: "Visa Logo" },
-  { src: mastercard, alt: "MasterCard Logo" },
-  { src: paypal, alt: "PayPal Logo" },
-  { src: applepay, alt: "Apple Pay Logo" },
-  { src: googlepay, alt: "Google Pay Logo" },
-];
 
 function FooterCopyright() {
   return (
     <footer className={styles.copyright}>
       <p>Outfitter &copy;2000-2024, All Rights Reserved</p>
       <div className={styles.payment_methods}>
-        {paymentMethods.map((method, index) => (
-          <Image key={index} unoptimized src={method.src} alt={method.alt} />
-        ))}
+        <PaymentMethods />
       </div>
     </footer>
   );
