7d0d2da24573f060598ef42b88fac7763c104272
Dimitar Ivanov <dimitar.ivanov.dev@gmail.com>
2024-12-02 00:39:29 +0200

    Create getAllImagesFromFolder function

--- src/app/lib/data-service.js
+++ src/app/lib/data-service.js
@@ -1,3 +1,23 @@
+import { supabase } from "./supabase";
+
+export async function getAllImagesFromFolder(folderName) {
+  const { data, error } = await supabase.storage.from(folderName).list();
+
+  if (error) {
+    console.error("Error fetching images:", error);
+    return [];
+  }
+
+  const images = data.map((item) => {
+    return {
+      name: item.name,
+      imageUrl: supabase.storage.from(folderName).getPublicUrl(`${item.name}`).data.publicUrl,
+    };
+  });
+
+  return images;
+}
+
 export const getAllProducts = async () => {
   const { data, error } = await supabase.from("products").select("*");
 
