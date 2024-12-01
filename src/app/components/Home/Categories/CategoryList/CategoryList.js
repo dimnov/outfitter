e3327e86e556894ab567903bbe4edae37ae6cd38
Dimitar Ivanov <dimitar.ivanov.dev@gmail.com>
2024-12-01 23:57:14 +0200

    Create CategoryList component

--- src/app/components/Home/Categories/CategoryList/CategoryList.js
+++ src/app/components/Home/Categories/CategoryList/CategoryList.js
@@ -1,28 +1,14 @@
-import Link from "next/link";
 import styles from "./CategoryList.module.css";
-import Image from "next/image";
+import CategoryItem from "../CategoryItem/CategoryItem";
+import { getAllImages } from "@/app/lib/data-service";
 
-import casual from "@images/categories/casual.png";
-import formal from "@images/categories/formal.png";
-import party from "@images/categories/party.png";
-import gym from "@images//categories/gym.png";
+async function CategoryList() {
+  const categories = await getAllImages();
 
-const categories = [
-  { id: "casual", src: casual, alt: "Casual category" },
-  { id: "formal", src: formal, alt: "Formal category" },
-  { id: "party", src: party, alt: "Party category" },
-  { id: "gym", src: gym, alt: "Gym category" },
-];
-
-function CategoryList() {
   return (
     <ul className={styles.categories_grid}>
       {categories.map((cat) => (
-        <li key={cat.id} className={`${styles.category_box} ${styles[cat.id]}`}>
-          <Link href="#">
-            <Image unoptimized src={cat.src} alt={cat.alt} />
-          </Link>
-        </li>
+        <CategoryItem key={cat.name} cat={cat} />
       ))}
     </ul>
   );
