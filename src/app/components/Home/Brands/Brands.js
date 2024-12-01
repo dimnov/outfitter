0c60fef2b090173469fd9f59aaaf2e2086dae7de
Dimitar Ivanov <dimitar.ivanov.dev@gmail.com>
2024-12-02 00:10:48 +0200

    Get brands images from supabase

--- src/app/components/Home/Brands/Brands.js
+++ src/app/components/Home/Brands/Brands.js
@@ -1,27 +1,15 @@
+import { getAllImagesFromFolder } from "@/app/lib/data-service";
 import styles from "./Brands.module.css";
-import Image from "next/image";
 
-import versace from "/public/images/brands/Versace.png";
-import zara from "/public/images/brands/Zara.png";
-import gucci from "/public/images/brands/Gucci.png";
-import prada from "/public/images/brands/Prada.png";
-import calvin from "/public/images/brands/Calvin.png";
+async function Brands() {
+  const brands = await getAllImagesFromFolder("brands");
 
-const brandImages = [
-  { src: versace, alt: "Versace Logo" },
-  { src: zara, alt: "Zara Logo" },
-  { src: gucci, alt: "Gucci Logo" },
-  { src: prada, alt: "Prada Logo" },
-  { src: calvin, alt: "Calvin Klein Logo" },
-];
-
-function Brands() {
   return (
     <section className={styles.section_brands}>
       <ul className={styles.brands_list}>
-        {brandImages.map((brand, index) => (
-          <li key={index} className={styles.brand_item}>
-            <Image unoptimized src={brand.src} alt={brand.alt} />
+        {brands.map((brand) => (
+          <li key={brand.name} className={styles.brand_item}>
+            <img src={brand.imageUrl} />
           </li>
         ))}
       </ul>
