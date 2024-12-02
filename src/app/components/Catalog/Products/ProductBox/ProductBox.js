736098418c16b6459937930021850541d1682e65
Dimitar Ivanov <dimitar.ivanov.dev@gmail.com>
2024-12-02 23:01:37 +0200

    remove the svgs

--- src/app/components/Catalog/Products/ProductBox/ProductBox.js
+++ src/app/components/Catalog/Products/ProductBox/ProductBox.js
@@ -1,86 +1,34 @@
 import Image from "next/image";
 import styles from "./ProductBox.module.css";
 
-import tshirt from "@images/products/tshirt.png";
+import StarScore from "@/app/components/Icons/StarScore";
 
-function ProductBox() {
+function ProductBox({ product }) {
+  const { title, image_url, price, score, discount_price } = product;
+
+  const discountPercentage = ((price - discount_price) / price) * 100;
+
   return (
     <div className={styles.product_box}>
-      <Image src={tshirt} unoptimized alt="tshirt" />
+      <Image src={image_url} width={295} height={298} alt="tshirt" />
       <div className={styles.product_info}>
-        <p className={styles.product_title}>Gradient Graphic T-shirt</p>
+        <p className={styles.product_title}>{title}</p>
         <div className={styles.score_box}>
           <div className={styles.score_stars}>
-            <svg
-              width="19"
-              height="17"
-              viewBox="0 0 19 17"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M9.24494 0.255005L11.8641 5.89491L18.0374 6.6431L13.4829 10.8769L14.679 16.9793L9.24494 13.956L3.8109 16.9793L5.00697 10.8769L0.452479 6.6431L6.62573 5.89491L9.24494 0.255005Z"
-                fill="#FFC633"
-              />
-            </svg>
-            <svg
-              width="19"
-              height="17"
-              viewBox="0 0 19 17"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M9.24494 0.255005L11.8641 5.89491L18.0374 6.6431L13.4829 10.8769L14.679 16.9793L9.24494 13.956L3.8109 16.9793L5.00697 10.8769L0.452479 6.6431L6.62573 5.89491L9.24494 0.255005Z"
-                fill="#FFC633"
-              />
-            </svg>
-            <svg
-              width="19"
-              height="17"
-              viewBox="0 0 19 17"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M9.24494 0.255005L11.8641 5.89491L18.0374 6.6431L13.4829 10.8769L14.679 16.9793L9.24494 13.956L3.8109 16.9793L5.00697 10.8769L0.452479 6.6431L6.62573 5.89491L9.24494 0.255005Z"
-                fill="#FFC633"
-              />
-            </svg>
-            <svg
-              width="19"
-              height="17"
-              viewBox="0 0 19 17"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M9.24494 0.255005L11.8641 5.89491L18.0374 6.6431L13.4829 10.8769L14.679 16.9793L9.24494 13.956L3.8109 16.9793L5.00697 10.8769L0.452479 6.6431L6.62573 5.89491L9.24494 0.255005Z"
-                fill="#FFC633"
-              />
-            </svg>
-            <svg
-              width="19"
-              height="17"
-              viewBox="0 0 19 17"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M9.24494 0.255005L11.8641 5.89491L18.0374 6.6431L13.4829 10.8769L14.679 16.9793L9.24494 13.956L3.8109 16.9793L5.00697 10.8769L0.452479 6.6431L6.62573 5.89491L9.24494 0.255005Z"
-                fill="#FFC633"
-              />
-            </svg>
+            <StarScore score={score} />
           </div>
-          <p className={styles.score_number}>5.0/5</p>
+          <p className={styles.score_number}>{score}/5</p>
         </div>
         <div className={styles.product_price_box}>
-          <p className={styles.product_price}>$145</p>
-
-          {/* if discount */}
-          <p className={styles.product_price_discount}>$160</p>
-          <p className={styles.discount_percentage}>-30%</p>
-          {/* if discount */}
+          {discount_price !== 0 ? (
+            <>
+              <p className={styles.product_price}>${discount_price}</p>
+              <p className={styles.product_price_discount}>${price}</p>
+              <p className={styles.discount_percentage}>-{discountPercentage.toFixed(0)}%</p>
+            </>
+          ) : (
+            <p className={styles.product_price}>${price}</p>
+          )}
         </div>
       </div>
     </div>
