0f49f9d95e172487ddebe77034c3d410da6aa563
Dimitar Ivanov <dimitar.ivanov.dev@gmail.com>
2024-12-02 23:03:21 +0200

    Move the Pagination component outside

--- src/app/(routes)/catalog/page.js
+++ src/app/(routes)/catalog/page.js
@@ -1,22 +1,17 @@
 import styles from "./catalog.module.css";
-import Breadcrumb from "@/app/components/Catalog/Breadcrumb/Breadcrumb";
 
+import Breadcrumb from "@/app/components/Catalog/Breadcrumb/Breadcrumb";
 import Filters from "@/app/components/Catalog/Filters/Filters";
-import Pagination from "@/app/components/Catalog/Pagination/Pagination";
 import Products from "@/app/components/Catalog/Products/Products";
 
-function page() {
+async function page() {
   return (
     <section className={styles.catalog}>
       <hr />
       <Breadcrumb />
       <div className={styles.main}>
         <Filters />
-        <div>
-          <Products />
-          <hr />
-          <Pagination />
-        </div>
+        <Products />
       </div>
     </section>
   );
