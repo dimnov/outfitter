86f33c6745fd2721dc9a446c0a61e11bdad938ed
Dimitar Ivanov <dimitar.ivanov.dev@gmail.com>
2025-01-17 23:26:29 +0200

    Add ProductPriceBox

--- src/app/components/Catalog/Products/ProductBox/ProductBox.js
+++ src/app/components/Catalog/Products/ProductBox/ProductBox.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import styles from "./ProductBox.module.css";
 
 import StarScore from "@/app/components/Icons/StarScore";
+import ProductPriceBox from "../ProductPriceBox/ProductPriceBox";
 
 function ProductBox({ product }) {
   const { title, image_url, price, score, discount_price } = product;
@@ -19,17 +20,11 @@ function ProductBox({ product }) {
           </div>
           <p className={styles.score_number}>{score}/5</p>
         </div>
-        <div className={styles.product_price_box}>
-          {discount_price !== 0 ? (
-            <>
-              <p className={styles.product_price}>${discount_price}</p>
-              <p className={styles.product_price_discount}>${price}</p>
-              <p className={styles.discount_percentage}>-{discountPercentage.toFixed(0)}%</p>
-            </>
-          ) : (
-            <p className={styles.product_price}>${price}</p>
-          )}
-        </div>
+        <ProductPriceBox
+          discount_price={discount_price}
+          price={price}
+          discountPercentage={discountPercentage}
+        />
       </div>
     </div>
   );
