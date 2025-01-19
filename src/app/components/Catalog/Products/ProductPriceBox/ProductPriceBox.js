879e6b8521d4252d6ba321356b9df248b12ec6b9
Dimitar Ivanov <dimitar.ivanov.dev@gmail.com>
2025-01-19 16:09:46 +0200

    add custom className

--- src/app/components/Catalog/Products/ProductPriceBox/ProductPriceBox.js
+++ src/app/components/Catalog/Products/ProductPriceBox/ProductPriceBox.js
@@ -1,8 +1,10 @@
 import styles from "./ProductPriceBox.module.css";
 
-function ProductPriceBox({ price, discountPercentage, discount_price, className = "" }) {
+function ProductPriceBox({ price, discount_price, className }) {
+  const discountPercentage = ((price - discount_price) / price) * 100;
+
   return (
-    <div className={`${styles.product_price_box} ${className}`}>
+    <div className={`${styles.product_price_box} ${className || ""}`}>
       {discount_price !== 0 ? (
         <>
           <p className={styles.product_price}>${discount_price}</p>
