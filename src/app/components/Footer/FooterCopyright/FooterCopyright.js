e44d3914001d23e8b64df6c45718a7fc5db8ace7
Dimitar Ivanov <dimitar.ivanov.dev@gmail.com>
2025-01-21 17:36:10 +0200

    Gets the current year

--- src/app/components/Footer/FooterCopyright/FooterCopyright.js
+++ src/app/components/Footer/FooterCopyright/FooterCopyright.js
@@ -2,9 +2,10 @@ import PaymentMethods from "../../Icons/PaymentMethods";
 import styles from "./FooterCopyright.module.css";
 
 function FooterCopyright() {
+  const currentYear = new Date().getFullYear();
   return (
     <footer className={styles.copyright}>
-      <p>Outfitter &copy;2000-2024, All Rights Reserved</p>
+      <p>Outfitter &copy;2000-{currentYear}, All Rights Reserved</p>
       <div className={styles.payment_methods}>
         <PaymentMethods />
       </div>
