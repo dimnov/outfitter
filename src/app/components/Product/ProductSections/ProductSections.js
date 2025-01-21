5da8bf8421cd4e09bcc3aa83d6f3d21d9382b3fa
Dimitar Ivanov <dimitar.ivanov.dev@gmail.com>
2025-01-21 17:30:01 +0200

    Update ProductSections.js

--- src/app/components/Product/ProductSections/ProductSections.js
+++ src/app/components/Product/ProductSections/ProductSections.js
@@ -1,11 +1,47 @@
+"use client";
+
+import { useState } from "react";
 import styles from "./ProductSections.module.css";
+import ProductSectionsContainer from "./ProductSectionsContainer/ProductSectionsContainer";
+import ProductSectionsButton from "./ProductSectionsButton/ProductSectionsButton";
+
+const BUTTONS = [
+  {
+    text: "Product Details",
+    value: "details",
+  },
+  {
+    text: "Rating & Reviews",
+    value: "reviews",
+  },
+  {
+    text: "FAQs",
+    value: "faq",
+  },
+];
 
 function ProductSections() {
+  const [activeSection, setActiveSection] = useState("reviews");
+
+  const handleSetActive = (section) => {
+    setActiveSection(section);
+  };
+
   return (
     <div className={styles.sections}>
-      <button>Product Details</button>
-      <button>Rating & Reviews</button>
-      <button>FAQs</button>
+      <div className={styles.buttons}>
+        {BUTTONS.map(({ text, value }) => (
+          <ProductSectionsButton
+            key={value}
+            text={text}
+            value={value}
+            isActive={activeSection === value}
+            onClick={handleSetActive}
+          />
+        ))}
+      </div>
+
+      <ProductSectionsContainer activeSection={activeSection} />
     </div>
   );
 }
