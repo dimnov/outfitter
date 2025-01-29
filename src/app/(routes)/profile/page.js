50d64407b5e4054cfacc336cd73696eca8cb838d
Dimitar Ivanov <dimitar.ivanov.dev@gmail.com>
2025-01-29 23:44:54 +0200

    Create profile page

---
+++ src/app/(routes)/profile/page.js
@@ -0,0 +1,45 @@
+import Link from "next/link";
+import styles from "./Profile.module.css";
+
+function page() {
+  return (
+    <section className={styles.profile}>
+      <div className={styles.form_container}>
+        <div className={styles.form_box_header}>
+          <h3>Welcome, Friend!</h3>
+          <p>Login to save your favorite products.</p>
+        </div>
+        <form className={styles.form_box}>
+          <div className={styles.form_box_inputs}>
+            <div className={styles.form_box_input}>
+              <label>Email</label>
+              <input type="email" placeholder="Your email here..." />
+            </div>
+            <div className={styles.form_box_input}>
+              <label>Password</label>
+              <input type="password" placeholder="Enter password..." />
+            </div>
+          </div>
+          <Link className={styles.forget_password} href={"#"}>
+            Forget Password?
+          </Link>
+          <div className={styles.form_box_btns}>
+            <div className={styles.checkbox_box}>
+              <input type="checkbox" id="checkbox" className={styles.checkbox} />
+              <label htmlFor="checkbox">
+                By creating an account, you agree to our <Link href={"#"}>Terms & Conditions</Link>.
+              </label>
+            </div>
+            <button>Login</button>
+          </div>
+        </form>
+        <div className={styles.account_btn_box}>
+          <p>Don&apos;t have an account?</p>
+          <button>Create Account</button>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default page;
