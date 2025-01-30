785a83e2fba0d2bde3d760306023b4c0934ede97
Dimitar Ivanov <dimitar.ivanov.dev@gmail.com>
2025-01-30 16:47:31 +0200

    Create login method

--- src/app/lib/auth.js
+++ src/app/lib/auth.js
@@ -10,3 +10,15 @@ export const register = async (email, password) => {
 
   return { user };
 };
+
+export const login = async (email, password) => {
+  console.log(email, password);
+  const { user, error } = await supabase.auth.signInWithPassword({ email, password });
+
+  if (error) {
+    console.error(error.message);
+    return { error: error.message };
+  }
+
+  return { user };
+};
