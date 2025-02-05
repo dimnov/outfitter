8c0c3ba237d6f86037157b1540493656e8084707
Dimitar Ivanov <dimitar.ivanov.dev@gmail.com>
2025-02-05 22:30:24 +0200

    Fetches data from the server component

--- src/app/(routes)/products/[productId]/page.js
+++ src/app/(routes)/products/[productId]/page.js
@@ -4,14 +4,33 @@ import ProductOffers from "@/app/components/Product/ProductOffers/ProductOffers"
 import Breadcrumbs from "@/app/components/Product/Breadcrumbs/Breadcrumbs";
 import ProductContainer from "@/app/components/Product/ProductContainer/ProductContainer";
 import { ProductProvider } from "@/app/context/ProductContext";
+import {
+  getAllProducts,
+  getProduct,
+  getProductImages,
+  getProductReviews,
+} from "@/app/lib/data-service";
+
+export async function generateStaticParams() {
+  const { products } = await getAllProducts();
+
+  const ids = products.map((product) => ({ productId: String(product.id) }));
+
+  return ids;
+}
 
 async function page({ params }) {
   const { productId } = await params;
 
+  const { productData } = await getProduct(productId);
+  const { images } = await getProductImages(productId);
+  const { reviews } = await getProductReviews(productId);
+  const category = productData.category;
+
   return (
     <section className={styles.product}>
       <hr />
-      <ProductProvider productId={productId}>
+      <ProductProvider value={{ productData, images, reviews, category }}>
         <Breadcrumbs productId={productId} />
         <ProductContainer />
         <ProductSections />
