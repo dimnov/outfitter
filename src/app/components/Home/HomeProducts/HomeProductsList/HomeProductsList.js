bc47c468db4fc6f16a83f9fd782c3b4d99074426
Dimitar Ivanov <dimitar.ivanov.dev@gmail.com>
2025-02-10 18:58:18 +0200

    Import the products box

--- src/app/components/Home/HomeProducts/HomeProductsList/HomeProductsList.js
+++ src/app/components/Home/HomeProducts/HomeProductsList/HomeProductsList.js
@@ -1,24 +1,18 @@
 import styles from "./HomeProductsList.module.css";
-import StarScore from "@/app/components/Icons/StarScore";
-import Image from "next/image";
 import Link from "next/link";
+import ProductBox from "@/app/components/Catalog/Products/ProductBox/ProductBox";
 
 function HomeProductsList({ products = [], title, path = "#" }) {
   return (
     <div className={styles.box_products}>
       <p className={styles.box_products_title}>{title}</p>
-      <ul className={styles.box_products_list}>
+      <div className={styles.box_products_list}>
         {products.map((product) => (
-          <li className={styles.product_item} key={product.id}>
-            <Link href={`products/${product.id}`}>
-              <Image width={295} height={298} src={product?.image_url} alt={product.title} />
-            </Link>
-            <p className={styles.product_item_title}>{product.title}</p>
-            <StarScore score={product.score} size={19} />
-            <p className={styles.product_item_price}>${product.price}</p>
-          </li>
+          <Link key={product.id} href={`/products/${product.id}`}>
+            <ProductBox product={product} />
+          </Link>
         ))}
-      </ul>
+      </div>
       <Link href={path} className={styles.view_all_button}>
         View All
       </Link>
