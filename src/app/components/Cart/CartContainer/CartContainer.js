c59d5d841db9f8488cd0c2eb906d4d1336286b07
Dimitar Ivanov <dimitar.ivanov.dev@gmail.com>
2025-02-15 21:00:35 +0200

    Create CartContainer component

---
+++ src/app/components/Cart/CartContainer/CartContainer.js
@@ -0,0 +1,35 @@
+"use client";
+
+import { useCart } from "@/app/context/CartContext";
+import CartProductsBox from "../CartProductsBox/CartProductsBox";
+import CartSummaryBox from "../CartSummaryBox/CartSummaryBox";
+import styles from "./CartContainer.module.css";
+import Link from "next/link";
+
+function CartContainer() {
+  const { cartLength } = useCart();
+
+  return (
+    <div className={styles.cart_container}>
+      {cartLength ? (
+        <>
+          <h3 className={styles.cart_title}>your cart</h3>
+
+          <div className={styles.cart_box}>
+            <CartProductsBox />
+            <CartSummaryBox />
+          </div>
+        </>
+      ) : (
+        <div className={styles.empty_cart}>
+          <h3 className={styles.cart_title}>your cart is empty</h3>
+          <Link href={"/shop"} className={styles.back_button}>
+            Go to Shop
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default CartContainer;
