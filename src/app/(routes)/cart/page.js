18b5576d016dbfee290e6382f2fd677b71b9d8e3
Dimitar Ivanov <dimitar.ivanov.dev@gmail.com>
2025-02-15 21:04:05 +0200

    Split code to CartContainer

--- src/app/(routes)/cart/page.js
+++ src/app/(routes)/cart/page.js
@@ -1,18 +1,13 @@
 import styles from "./Cart.module.css";
 import Breadcrumbs from "@/app/components/Catalog/Breadcrumbs/Breadcrumbs";
-import CartProductsBox from "@/app/components/Cart/CartProductsBox/CartProductsBox";
-import CartSummaryBox from "@/app/components/Cart/CartSummaryBox/CartSummaryBox";
+import CartContainer from "@/app/components/Cart/CartContainer/CartContainer";
 
 function cart() {
   return (
     <section className={styles.cart}>
       <hr />
       <Breadcrumbs path={"Cart"} />
-      <h3 className={styles.cart_title}>your cart</h3>
-      <div className={styles.cart_container}>
-        <CartProductsBox />
-        <CartSummaryBox />
-      </div>
+      <CartContainer />
     </section>
   );
 }
