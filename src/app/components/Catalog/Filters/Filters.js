d8078f04d33d84325ca54f13942beb0b442157c0
Dimitar Ivanov <dimitar.ivanov.dev@gmail.com>
2025-02-16 20:17:15 +0200

    Import the filters

--- src/app/components/Catalog/Filters/Filters.js
+++ src/app/components/Catalog/Filters/Filters.js
@@ -1,6 +1,10 @@
 import SettingsIcon from "../../Icons/SettingsIcon";
+import ColorsFilter from "./ColorsFilter/ColorsFilter";
 import FilterBox from "./FilterBox/FilterBox";
 import styles from "./Filters.module.css";
+import PriceFilter from "./PriceFilter/PriceFilter";
+import SizeFilter from "./SizeFilter/SizeFilter";
+import StyleFilter from "./StyleFilter/StyleFilter";
 
 function Filters() {
   return (
@@ -15,6 +19,16 @@ function Filters() {
         <FilterBox title={"T-shirts"} />
         <FilterBox title={"Shorts"} />
         <FilterBox title={"Jeans"} />
+
+        <hr className={styles.filter_line} />
+        <PriceFilter />
+        <hr className={styles.filter_line} />
+        <ColorsFilter />
+        <hr className={styles.filter_line} />
+        <SizeFilter />
+        <hr className={styles.filter_line} />
+        <StyleFilter />
+        <hr className={styles.filter_line} />
       </ul>
 
       <button className={styles.button}>Apply Filter</button>
