8ec17c44e8503e292f49167ecef2bf1dbc4e02c2
Dimitar Ivanov <dimitar.ivanov.dev@gmail.com>
2025-02-17 19:55:07 +0200

    pass the colors to colors filter

--- src/app/(routes)/shop/page.js
+++ src/app/(routes)/shop/page.js
@@ -2,14 +2,17 @@ import styles from "./catalog.module.css";
 import Breadcrumbs from "@/app/components/Catalog/Breadcrumbs/Breadcrumbs";
 import Filters from "@/app/components/Catalog/Filters/Filters";
 import Products from "@/app/components/Catalog/Products/Products";
+import { getAllColors } from "@/app/lib/data-service";
 
 async function page() {
+  const { colors } = await getAllColors();
+
   return (
     <section className={styles.catalog}>
       <hr />
       <Breadcrumbs path={"Shop"} />
       <div className={styles.main}>
-        <Filters />
+        <Filters colors={colors}/>
         <Products />
       </div>
     </section>
--- src/app/components/Catalog/Filters/ColorsFilter/ColorsFilter.js
+++ src/app/components/Catalog/Filters/ColorsFilter/ColorsFilter.js
@@ -3,43 +3,11 @@
 import { useState } from "react";
 import styles from "./ColorsFilter.module.css";
 import FilterOptionsBox from "../FilterOptionsBox/FilterOptionsBox";
+import ColorCircleSVG from "@/app/components/Icons/ColorCircleSVG";
 
-const COLORS = [
-  {
-    color_hex: "31344F",
-  },
-  {
-    color_hex: "4F4531",
-  },
-  {
-    color_hex: "316F4A",
-  },
-  {
-    color_hex: "317F4A",
-  },
-  {
-    color_hex: "318F4A",
-  },
-  {
-    color_hex: "319F4A",
-  },
-  {
-    color_hex: "312F4A",
-  },
-  {
-    color_hex: "311F4A",
-  },
-  {
-    color_hex: "361F4A",
-  },
-  {
-    color_hex: "371F4A",
-  },
-];
-
-function ColorsFilter() {
+function ColorsFilter({ onClick, colors }) {
   const [isOpen, setIsOpen] = useState(false);
-  const [selectedColor, setSelectedColor] = useState("31344F");
+  const [selectedColor, setSelectedColor] = useState("");
 
   const toggleIsOpen = () => {
     setIsOpen((prev) => !prev);
@@ -47,6 +15,7 @@ function ColorsFilter() {
 
   const changeSelectedColor = (color) => {
     setSelectedColor(color);
+    onClick("color", color);
   };
 
   return isOpen ? (
@@ -68,35 +37,13 @@ function ColorsFilter() {
       </div>
 
       <ul className={styles.colors_list}>
-        {COLORS.map(({ color_hex }) => (
+        {colors.map((color_hex) => (
           <li
             key={color_hex}
             className={selectedColor === color_hex ? styles.selected : ""}
             onClick={() => changeSelectedColor(color_hex)}
           >
-            <svg
-              className={styles.circle}
-              width="37"
-              height="37"
-              viewBox="0 0 37 37"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <circle cx="18.5" cy="18.5" r="18.5" fill={`#${color_hex}`} />
-            </svg>
-            <svg
-              className={styles.checkmark}
-              width="14"
-              height="11"
-              viewBox="0 0 14 11"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M13.5306 2.03063L5.5306 10.0306C5.46092 10.1005 5.37813 10.156 5.28696 10.1939C5.1958 10.2317 5.09806 10.2512 4.99935 10.2512C4.90064 10.2512 4.8029 10.2317 4.71173 10.1939C4.62057 10.156 4.53778 10.1005 4.4681 10.0306L0.968098 6.53063C0.898333 6.46087 0.842993 6.37804 0.805236 6.28689C0.76748 6.19574 0.748047 6.09804 0.748047 5.99938C0.748047 5.90072 0.76748 5.80302 0.805236 5.71187C0.842993 5.62072 0.898333 5.53789 0.968098 5.46813C1.03786 5.39837 1.12069 5.34302 1.21184 5.30527C1.30299 5.26751 1.40069 5.24808 1.49935 5.24808C1.59801 5.24808 1.69571 5.26751 1.78686 5.30527C1.87801 5.34302 1.96083 5.39837 2.0306 5.46813L4.99997 8.4375L12.4693 0.969379C12.6102 0.828483 12.8013 0.749329 13.0006 0.749329C13.1999 0.749329 13.391 0.828483 13.5318 0.969379C13.6727 1.11028 13.7519 1.30137 13.7519 1.50063C13.7519 1.69989 13.6727 1.89098 13.5318 2.03188L13.5306 2.03063Z"
-                fill="white"
-              />
-            </svg>
+            <ColorCircleSVG color_hex={color_hex} isSelected={selectedColor === color_hex} />
           </li>
         ))}
       </ul>
