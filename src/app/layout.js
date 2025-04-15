e5e642ccc1294a6a5ef3f24642a9271a8667e44e
Dimitar Ivanov <dimitar.ivanov.dev@gmail.com>
2025-04-15 21:40:13 +0300

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,6 +28,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body>
         <AuthProvider>
           <CartProvider>
